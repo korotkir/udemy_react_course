ca8d1dee91a30af3d4b9de4921a622c139c4361c
tre3ubec <ekoroteev27@yandex.ru>
2022-01-26 23:04:29 +0300

    Starting React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Layout from "./hoc/Layout/Layout";
+import Layout from './hoc/Layout/Layout'
 import Quiz from './containers/Quiz/Quiz.js'
+import QuizList from './containers/QuizList/QuizList'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
+import Auth from './containers/Auth/Auth'
+import {Routes, Route} from 'react-router-dom'
+
 
 function App() {
-  return (
-    <Layout>
-     <Quiz />
-    </Layout>
-  );
+ return (
+  <Layout>
+    <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/quiz-creator' element={<QuizCreator />} />
+        <Route path='/quiz/:id' element={<Quiz />} />
+        <Route path='/' element={<QuizList />} />
+    </Routes>
+  </Layout>
+ );
 }
 
 export default App;
