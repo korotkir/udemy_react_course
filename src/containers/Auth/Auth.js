b0f95b477cbb285e08047db8d465ba33b224d243
tre3ubec <ekoroteev27@yandex.ru>
2022-01-27 21:34:04 +0300

    start Form Practic

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react'
+import styles from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={styles.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form className={styles.AuthForm} onSubmit={this.submitHandler}>
+                        <Input
+                            label="Email"
+                        />
+                        <Input
+                            label="Пароль"
+                            errorMessage={'TEST'}
+                        />
+
+                        <Button type="success" onClick={this.loginHandler}>Войти</Button>
+                        <Button type="primary" onCloick={this.registerHandler}>Зарегистрироваться</Button>
+                    </form>
+                </div>
             </div>
         )
     }
