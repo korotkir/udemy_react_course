61bae7f922b32bbd6660e1e9d2585898625675a4
tre3ubec <ekoroteev27@yandex.ru>
2022-01-27 20:02:31 +0300

    React Router final

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import styles from './FinishedQuiz.module.css'
-import Button from "../UI/Button/Button";
+import Button from '../UI/Button/Button'
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz = props => {
   const succesCount = Object.keys(props.results).reduce((total, key) => {
@@ -9,49 +10,52 @@ const FinishedQuiz = props => {
    return total
   }, 0)
 
-  return (
-    <div className={styles.FinishedQuiz}>
-      <ul>
-       { props.quiz.map((quizItem, index) => {
+    return (
+        <div className={styles.FinishedQuiz}>
+            <ul>
+                {props.quiz.map((quizItem, index) => {
 
-        const cls = [
-         'fa',
-         props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
-         styles[props.results[quizItem.id]]
-        ]
+                    const cls = [
+                        'fa',
+                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                        styles[props.results[quizItem.id]]
+                    ]
 
-        return(
-         <li
-          key={index}
-         >
-          <strong>{index + 1}</strong>.&nbsp;
-          {quizItem.question}
-          <i className={cls.join(' ')} />
+                    return (
+                        <li
+                            key={index}
+                        >
+                            <strong>{index + 1}</strong>.&nbsp;
+                            {quizItem.question}
+                            <i className={cls.join(' ')}/>
 
-         </li>
-        )
+                        </li>
+                    )
 
-       })}
+                })}
 
-        {/*<li>*/}
-        {/*  <strong>1.</strong>*/}
-        {/*  How are you?*/}
-        {/*  <i className={'fa fa-times error'} />*/}
-        {/*</li>*/}
-        {/*<li>*/}
-        {/*  <strong>1.</strong>*/}
-        {/*  How are you?*/}
-        {/*  <i className={'fa fa-check success'} />*/}
-        {/*</li>*/}
-      </ul>
-      <p>Правильно {succesCount} из {props.quiz.length}</p>
+                {/*<li>*/}
+                {/*  <strong>1.</strong>*/}
+                {/*  How are you?*/}
+                {/*  <i className={'fa fa-times error'} />*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*  <strong>1.</strong>*/}
+                {/*  How are you?*/}
+                {/*  <i className={'fa fa-check success'} />*/}
+                {/*</li>*/}
 
-      <div>
-        <Button onClick={props.onRetry} type='primary'>Повторить</Button>
-        <Button onClick={props.onRetry} type='success'>Перейти в список тестов</Button>
-      </div>
-    </div>
-  )
+            </ul>
+            <p>Правильно {succesCount} из {props.quiz.length}</p>
+
+            <div>
+                <Button onClick={props.onRetry} type="primary">Повторить</Button>
+                <Link to={'/'}>
+                    <Button onClick={props.onRetry} type="success">Перейти в список тестов</Button>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default FinishedQuiz
--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react'
+import styles from './QuizList.module.css'
+import {NavLink} from 'react-router-dom'
 
 export default class QuizList extends Component {
+
+    renderQuizes() {
+        return [1,2,3].map((quiz,index) => {
+            return (
+                <li
+                    key={index}
+                >
+                    <NavLink to={'/quiz/' + quiz}>
+                        Test {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>QuizList</h1>
+            <div className={styles.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
             </div>
         )
     }
